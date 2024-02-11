c8b3eadd5b2bedf3750d45d042ded792f4776636
EswarSudhan <skeswarsudhan@gmail.com>
2024-02-12 00:57:25 +0530

    c1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 // src/components/BacterialCharacteristicsForm.js
 import React, { useState } from 'react';
 import axios from "axios";
+import { Hourglass } from 'react-loader-spinner';
 import './home.css';
 
 const BacterialCharacteristicsForm = () => {
@@ -11,6 +12,7 @@ const BacterialCharacteristicsForm = () => {
     nonLactoseFermenting: false,
     colistinResistant: false,
   };
+  const [isLoading, setIsLoading] = useState(false); // Initially set to false
   const [characteristics, setCharacteristics] = useState(initialCharacteristicsState);
   const [image, setImage] = useState(null);
   const [imagePreview, setImagePreview] = useState(null);
@@ -19,8 +21,12 @@ const BacterialCharacteristicsForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    setIsLoading(true); // Set loading to true when the request starts
+
     const formData = new FormData();
-    formData.append('image', image);
+    if (image) {
+      formData.append('image', image);
+    }
 
     // Do not stringify characteristics, append them directly
     formData.append('gramNegativeBacilli', characteristics.gramNegativeBacilli);
@@ -39,19 +45,16 @@ const BacterialCharacteristicsForm = () => {
       console.log(response.data);
 
       // Check the server response and update state accordingly
-      if (response.data.success) {
-        setSubmissionMessage('Success: All characteristics are true');
-      } else {
-        setSubmissionMessage('Probability in zero: At least one characteristic is false');
-      }
+      setSubmissionMessage(response.data.message);
 
       setCharacteristics(initialCharacteristicsState); // Clear form state
       setImage(null);
     } catch (error) {
       console.error('Error submitting form:', error);
+    } finally {
+      setIsLoading(false); // Set loading to false after the request completes (success or error)
     }
   };
- 
 
   const handleCheckboxChange = (characteristic) => {
     setCharacteristics((prevCharacteristics) => ({
@@ -71,84 +74,52 @@ const BacterialCharacteristicsForm = () => {
     setImage(selectedImage);
     // Create a preview URL for the selected image
     setImagePreview(URL.createObjectURL(selectedImage));
+    setSubmissionMessage("");
   };
 
+  const imgUrl = process.env.PUBLIC_URL + '/bg_1.jpeg';
+  const imgUrl1 = process.env.PUBLIC_URL + '/logo.png';
+
   return (
+    <div>
+
+      <img className="background_image" src={imgUrl} alt="Background" />
+      <img className="top-left-image" src={imgUrl1} alt="Background" />
     <div className="form-container">
-       <div className="refresh-button">
-        <button onClick={handleRefresh}>Refresh</button>
-      </div>
-      <h1>Bacterial Characteristics Form</h1>
+    
+      
+      <h1>Bacteria Classifier</h1>
       {submissionMessage && <p className="submission-message">{submissionMessage}</p>}
       <form onSubmit={handleSubmit}>
-        <div className="checkbox-group">
-          <label>
-            Gram Negative Bacilli:
-            <input
-              type="checkbox"
-              checked={characteristics.gramNegativeBacilli}
-              onChange={() => handleCheckboxChange('gramNegativeBacilli')}
-            />
-          </label>
-        </div>
-        <div className="checkbox-group">
-          <label>
-          oxidase Positive:
-            <input
-              type="checkbox"
-              checked={characteristics.oxidasePositive}
-              onChange={() => handleCheckboxChange('oxidasePositive')}
-            />
-          </label>
-        </div>
-        <div className="checkbox-group">
-          <label>
-          catalase Positive:
-            <input
-              type="checkbox"
-              checked={characteristics.catalasePositive}
-              onChange={() => handleCheckboxChange('catalasePositive')}
-            />
-          </label>
-        </div>
-        <div className="checkbox-group">
-          <label>
-          nonLactose Fermenting:
-            <input
-              type="checkbox"
-              checked={characteristics.nonLactoseFermenting}
-              onChange={() => handleCheckboxChange('nonLactoseFermenting')}
-            />
-          </label>
-        </div>
-
-        <div className="checkbox-group">
-          <label>
-          colistin Resistant:
-            <input
-              type="checkbox"
-              checked={characteristics.colistinResistant}
-              onChange={() => handleCheckboxChange(' colistinResistant')}
-            />
-          </label>
-        </div>
+      <div className='load'>
+      {isLoading && (
+         <Hourglass/>
+      )}
+      </div>
        
         <div className="image-upload">
           <label>
-            Upload Image:
+            Upload Image :
             <input
               type="file"
               accept="image/*"
               onChange={handleImageChange}
+              
+              
             />
           </label>
           {imagePreview && (
-            <img src={imagePreview} alt="Image Preview" className="image-preview" />
+            <img src={imagePreview} alt="Image Preview" className="image-upload" />
           )}
         </div>
+        <div className="refresh-button">
         <button type="submit" className="submit-button">Submit</button>
         
+        <button onClick={handleRefresh} className="submit-button">Refresh</button>
+      </div>
+        
       </form>
+    </div>
     </div>
   );
 };
